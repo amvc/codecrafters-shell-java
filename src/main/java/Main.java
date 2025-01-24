9b96edfc030bb6d74346406391e01efff8c0acff
Alberto Velázquez <amvc@amvc.mx>
2025-01-24 10:59:09 -0600

    The exit builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,10 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
 
+            if (input.equals("exit 0")) {
+                System.exit(0);
+            }
+
             System.out.println(input + ": command not found");
         }
     }
