940148242c0906e8da9d561a906a806fc69f690d
Alberto Velázquez <amvc@amvc.mx>
2025-02-06 20:46:10 -0600

    The echo builtin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -8,11 +10,23 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
 
-            if (input.equals("exit 0")) {
-                System.exit(0);
-            }
+            String[] parts = input.split(" ");
 
-            System.out.println(input + ": command not found");
+            switch (parts[0]) {
+                case "echo":
+                    String result = Stream.of(parts)
+                        .skip(1) // do not repeat "echo"
+                        .collect(Collectors.joining(" "));
+                    System.out.println(result);
+                    break;
+                case "exit":
+                    if (parts.length == 2 && parts[1].equals("0")) {
+                        System.exit(0);
+                    }
+                    // Fall-through in case the command is different from "exit 0"
+                default:
+                    System.out.println(input + ": command not found");
+            }
         }
     }
 }
